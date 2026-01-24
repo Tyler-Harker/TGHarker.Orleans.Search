889921bc22ee6bec80a7eced03a274bf9602fde8
Tyler Harker <tyler.harker@techfabric.com>
2026-01-24 10:38:33 -0700

    chore: added documentation website

---
+++ docs/theme.config.tsx
@@ -0,0 +1,46 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span style={{ fontWeight: 700 }}>Orleans.Search</span>,
+  project: {
+    link: 'https://github.com/TGHarker/TGHarker.Orleans.Search',
+  },
+  docsRepositoryBase: 'https://github.com/TGHarker/TGHarker.Orleans.Search/tree/master/docs',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - Orleans.Search'
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="TGHarker.Orleans.Search" />
+      <meta property="og:description" content="Full-text and indexed search capabilities for Microsoft Orleans grains" />
+    </>
+  ),
+  primaryHue: 270,
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
+  },
+  toc: {
+    float: true,
+    backToTop: true,
+  },
+  editLink: {
+    text: 'Edit this page on GitHub',
+  },
+  feedback: {
+    content: 'Question? Give us feedback',
+    labels: 'documentation',
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} - TGHarker.Orleans.Search
+      </span>
+    ),
+  },
+}
+
+export default config
